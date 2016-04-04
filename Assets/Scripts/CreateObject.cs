0cdfbf990dd5505e8d42dd500d29a9805d7c0063
Cole Arnold <max7black@gmail.com>
2016-04-03 22:35:09 -0400

    First real commit
    This commit is already after some of the basics to the game, like the
    boundaries, object creation, object movement and click and drag.

---
+++ Assets/Scripts/CreateObject.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateObject : MonoBehaviour
+{
+    public Sprite sprite;          // variable for the sprite we want
+
+    void Awake()
+    {
+        sprite = Resources.Load<Sprite>("Sprites/RedOval");
+
+    }
+
+    void Start()
+    {
+
+        // initalize the object 
+        GameObject matchObject = new GameObject("Object 1");
+
+        // Assign the sprite we want to the object
+        matchObject.AddComponent<SpriteRenderer>();
+        matchObject.GetComponent<SpriteRenderer>().sprite = sprite;
+
+        // Change the spawn loaction to be random
+        matchObject.transform.position = new Vector3(Random.Range(-15.0f, 15.0f), Random.Range(-4.0f, 4.0f), 0);
+
+        // Add the Collider to our object
+        matchObject.AddComponent<PolygonCollider2D>();
+
+        // Add the Rigidbody2D to our object so it's affected by physics
+        matchObject.AddComponent<Rigidbody2D>();
+        matchObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+        // Add the Move script to our object
+        matchObject.AddComponent<Move>();
+
+        // Add the ClickAndDrag script to our object
+        matchObject.AddComponent<ClickAndDrag>();
+
+
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    private float x_velocity;       // speed of object in the x direction
+    private float y_velocity;       // speed of object in the y direction
+
+    void Start()
+    {
+
+        x_velocity = Random.Range(-0.10f, 0.10f);
+        y_velocity = Random.Range(-0.10f, 0.10f);
+        transform.position = new Vector3(Random.Range(-15.0f, 15.0f), Random.Range(-4.0f, 4.0f), 0);
+    }
+
+    // Moves on the screen the object based on the x_velocity and y_velocity
+    void Update()
+    {   
+        transform.position = new Vector3(transform.position.x + x_velocity, transform.position.y + y_velocity, transform.position.z);
+    }
+
+    // If we hit the left or right boundary, invert x direction.
+    // If we hit the top or bottom boundary, invert y direction.
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        var name = collision.gameObject.name;
+        if (name == "RightCollider" || name == "LeftCollider") {
+            x_velocity *= -1;
+        }
+        if (name == "TopCollider" || name == "BottomCollider")
+        {
+            y_velocity *= -1;
+        }
+
+    }
+}
