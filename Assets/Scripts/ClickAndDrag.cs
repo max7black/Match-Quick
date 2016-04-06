0eb577fc883cc89f0d26d5844b53028b43249584
Cole Arnold <max7black@gmail.com>
2016-04-06 18:14:27 -0400

    Added Sprite Assets
    Add a blue, green, red, orange and yellow square to the Sprites folder.

--- Assets/Scripts/ClickAndDrag.cs
+++ Assets/Scripts/ClickAndDrag.cs
@@ -9,6 +9,7 @@ public class ClickAndDrag : MonoBehaviour
     private Vector3 screenPoint;
     private Vector3 offset;
 
+    // Entered when mouse is clicked and checks if mouse was clicked on the gameobject.
     void OnMouseDown()
     {
         screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
@@ -17,6 +18,8 @@ public class ClickAndDrag : MonoBehaviour
 
     }
 
+    // Enter if the mouse if dragged after being clicked and If the mouse was clicked on the object it changes the objects position 
+    // to be the same as the cursors.
     void OnMouseDrag()
     {
         Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
