e23775e4ddf0f3b9fc115ecdf38366d3b514b474
Cole Arnold <max7black@gmail.com>
2016-04-24 23:17:04 -0400

    More backgrounds, font, and main menu
    Added background and fonts to GameOver scene. Also started main menu
    with buttons and title.

---
+++ Assets/Scripts/GameMechanics/PlayButton.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayButton : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void ButtonClicked()
+    {
+        SceneManager.LoadScene("In-game");
+    }
+
+}
